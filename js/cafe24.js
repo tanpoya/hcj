97154e993f7ed3b5187e207fe7979869e10dc035
tanpoya <tanpoya02@gmail.com>
2023-02-07 22:32:34 +0900

    1

--- js/cafe24.js
+++ js/cafe24.js
@@ -7,4 +7,11 @@ $(() => {
     //         console.log($(text[i]).text());
     //     });
     // }
+
+    const flag_us = $(".flag-us");
+    const flag_jp = $(".flag-jp");
+    const flag_ch = $(".flag-ch");
+
+    
+
 })
